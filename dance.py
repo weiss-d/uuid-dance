88a9ad66cbd9a99fd8a99df8debb4830877d5c8e
Simon Weiss <wonder@simonweiss.space>
2020-09-15 10:46:35 +0300

    :sparkles: First variant of desired look.

--- dance.py
+++ dance.py
@@ -1,34 +1,53 @@
+import sys
+from typing import Any, List, Tuple
 import uuid
 
 import matplotlib.animation as animation
-import matplotlib.pyplot as mplt
-import numpy as np
+from matplotlib.lines import Line2D
+from matplotlib import pyplot
 
 
-mplt.style.use("dark_background")
-figure = mplt.figure()
-axes = mplt.axes(xlim=(0, 35), ylim=(0, 15))
-(line,) = axes.plot([], [], lw=2)
+def uuid_as_list() -> List[int]:
+    """Returns a list of integers each representing a symbol of random UUID.
+    Dashes are replaced with zeros.
+    """
+    return [int(i, 16) for i in str(uuid.uuid4()).replace("-", "0")]
 
 
-def init():
-    line.set_data([], [])
-    return (line,)
+pyplot.style.use("dark_background")
+figure = pyplot.figure()
+axes = pyplot.axes(xlim=(0, 35), ylim=(0, 15))
+(line_1,) = axes.plot([], [], linewidth=2.0, color="#00F5EA", drawstyle="steps-mid")
+(line_2,) = axes.plot([], [], linewidth=1.0, color="#0085F5", drawstyle="steps-mid")
+(line_3,) = axes.plot([], [], linewidth=0.5, color="#000BF5", drawstyle="steps-mid")
+
+uuid_queue = [uuid_as_list()] * 3
+x_data = [x for x in range(36)]
 
 
-xdata = [x for x in range(36)]#np.arange(0, 35)
+def init() -> Tuple[Line2D]:
+    """Animation init."""
+    line_1.set_data([], [])
+    line_2.set_data([], [])
+    line_3.set_data([], [])
+    return (line_3, line_2, line_1)
 
-ydata = []
 
-
-def animate(_):
-    ydata = [int(i, 16) for i in str(uuid.uuid4()).replace("-", "0")]
-    line.set_data(xdata, ydata)
-    return (line,)
+def animate(_: Any) -> Tuple[Line2D]:
+    """Animation frame."""
+    line_1.set_data(x_data, uuid_queue.pop(0))
+    line_2.set_data(x_data, uuid_queue[0])
+    line_3.set_data(x_data, uuid_queue[1])
+    uuid_queue.append(uuid_as_list())
+    return (line_3, line_2, line_1)
 
 
 dance = animation.FuncAnimation(
-    figure, animate, init_func=init, frames=500, interval=90, blit=True
+    figure, animate, init_func=init, frames=500, interval=100, blit=True
 )
-mplt.axis("off")
-mplt.show()
+pyplot.axis("off")
+
+if sys.argv[1] == "save":
+    dance.save(sys.argv[2] + ".gif", writer="imagemagick")
+elif sys.argv[1] == "show":
+    pyplot.show()
